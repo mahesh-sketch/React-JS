c0811b604b5d62e86f5cff52362d4c8e2c38dba5
mahesh dash <maheshdash84821@gmail.com>
2023-10-20 14:10:04 +0530

    Changes made

--- reactflowandstructure/src/main.jsx
+++ reactflowandstructure/src/main.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+function Mahesh(){
+  return (
+    <>
+     <h1>Mahesh kumar Dash</h1>
+    </>
+  )
+}
+
+/*const ReactElement = {
+  type : 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank',
+  },
+  children: 'Click me to visit google'
+}*/
+
+const ReactElement = React.createElement(
+  'a',
+  {
+    href : 'https://google.com',target: '_blank'},
+    'click me to visit google'
+)
+
+const NewReactElement = (
+  <a href="https://google.com" target="_blank">Click me to visit google</a>
+)
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  ReactElement
 )
